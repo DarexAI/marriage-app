7d68d3958462c651c68dab5b85f6c681e2476a9f
Milansingh09 <ms1318@srmist.edu.in>
2026-03-02 15:13:48 +0530

    sdk 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: true,
     host: 'localhost',
-    port: 5173,
-    
+    port: 5173
   }
 })
